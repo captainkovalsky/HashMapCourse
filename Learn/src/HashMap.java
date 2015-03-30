bf7d284a1cb55c601e860caae498d05d88c1577c
Roman <victor.dzundza.dev@gmail.com>
2015-03-31 01:09:48 +0300

    added get

--- Learn/src/HashMap.java
+++ Learn/src/HashMap.java
@@ -3,7 +3,7 @@ interface IHashMapable {
 
     int size();
 
-    HashableObject get(Long code);
+    HashableObject get(int code);
 }
 
 public class HashMap implements IHashMapable {
@@ -11,14 +11,14 @@ public class HashMap implements IHashMapable {
 
     public HashableObject[] table = new HashableObject[MAX_SIZE];
 
-    public int getCode(HashableObject object) {
-        return (object.getKey() % MAX_SIZE);
+    public int getCode(int key) {
+        return (key % MAX_SIZE);
     }
 
     @Override
     public boolean put(HashableObject toPut) throws Exception {
 
-        int code = getCode(toPut);
+        int code = getCode(toPut.getKey());
         boolean hasValue = table[code] != null;
 
         if (!hasValue) {
@@ -27,15 +27,17 @@ public class HashMap implements IHashMapable {
         } else {
             //check free space
             boolean hasFreeSpace = this.size() < MAX_SIZE;
+
             if (!hasFreeSpace) {
                 throw new Exception("Table is full.");
             } else {
                 int startFindFrom = code;
                 boolean isInserted = false;
                 boolean hasValueInNextPosition = false;
-                while(++startFindFrom <= MAX_SIZE){
-                     hasValueInNextPosition = table[startFindFrom] == null;
-                    if(!hasValueInNextPosition){
+                while (++startFindFrom <= MAX_SIZE) {
+                    hasValueInNextPosition = table[startFindFrom] == null;
+
+                    if (!hasValueInNextPosition) {
                         //insert here
                         table[startFindFrom] = toPut;
                         isInserted = true;
@@ -43,11 +45,12 @@ public class HashMap implements IHashMapable {
                     }
                 }
 
-                if(!isInserted){
+                if (!isInserted) {
                     startFindFrom = 0;
-                    while(startFindFrom < code){
+                    while (startFindFrom < code) {
                         hasValueInNextPosition = table[++startFindFrom] == null;
-                        if(!hasValueInNextPosition){
+
+                        if (!hasValueInNextPosition) {
                             table[startFindFrom] = toPut;
                             isInserted = true;
                             break;
@@ -66,15 +69,50 @@ public class HashMap implements IHashMapable {
         int size = 0;
         int i = 0;
         for (; i < MAX_SIZE; i++) {
+
             if (table[i] != null) {
                 size++;
             }
+
         }
         return size;
     }
 
     @Override
-    public HashableObject get(Long code) {
+    public HashableObject get(int key) {
+        int code = getCode(key);
+        int index;
+        boolean hasValue = table[code] != null;
+
+        if (!hasValue) {
+            return null;
+        }
+
+
+        if (table[code].getKey() == key) {
+            return table[code];
+        }
+
+        index = code == MAX_SIZE - 1 ? 0 : code + 1;
+
+        while ((index != -1) && (index != code)) {
+
+            hasValue = table[index] != null;
+
+            if (!hasValue) {
+                return null;
+            }
+
+            if (table[index].getKey() == key) {
+                return table[index];
+            }
+
+            if (index == MAX_SIZE - 1) {
+                index = 0;
+            } else {
+                index++;
+            }
+        }
         return null;
     }
 }
