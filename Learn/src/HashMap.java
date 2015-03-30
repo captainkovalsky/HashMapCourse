ecc10170ac87247f22c8e38ae6194ada7aec699f
Roman <victor.dzundza.dev@gmail.com>
2015-03-31 00:22:22 +0300

    added

--- Learn/src/HashMap.java
+++ Learn/src/HashMap.java
@@ -1,10 +1,52 @@
-/**
- * Created by vdzundza on 31.03.15.
- */
-public class HashMap {
+
+interface IHashMapable{
+     Boolean put(HashableObject toPut);
+     int size();
+     HashableObject get(Long code);
+}
+
+public class HashMap implements  IHashMapable{
     public final int MAX_SIZE = 1000;
 
-    public Object[] array = new Object[MAX_SIZE];
+    public HashableObject[] table = new HashableObject[MAX_SIZE];
+
+    public int code(HashableObject object)
+    {
+        return (object.getKey() % MAX_SIZE);
+    }
+
+    @Override
+    public Boolean put(HashableObject toPut) {
+        int probe;
 
 
+        int code = code(toPut);
+
+            if (code == (table.length - 1))
+                probe = 0;
+            else
+                probe = code + 1;
+
+
+        while ( (probe != -1) &&
+                (probe != code) )
+        {
+                if (probe == (table.length -1 ) )
+                    probe = 0;
+                else
+                    probe++;
+        }
+
+        return probe == -1;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public HashableObject get(Long code) {
+        return null;
+    }
 }
---
+++ Learn/src/HashableObject.java
@@ -0,0 +1,19 @@
+
+public class HashableObject {
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    private Integer key;
+    private Long value;
+
+    public HashableObject(Long value, Integer key) {
+        this.value = value;
+        this.key = key;
+    }
+}
